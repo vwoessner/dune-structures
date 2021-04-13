7b4de4c5a0e679e25f965548cab110acf1af8ae4
Lukas Riedel <mail@lukasriedel.com>
2021-04-13 10:54:14 +0000

    Update default build and add testing
    * Make app executables part of the default target and depend on
      generated operator code.
    * Raise minimum required CMake version to v3.16.
    * Add testing by executing config files in
      experiments/hansbo_reproduction.
    * List Python source files explicitly instead of globbing.
    * Remove spurious globaL filesystem library link.

--- cmake/modules/DuneStructuresMacros.cmake
+++ cmake/modules/DuneStructuresMacros.cmake
@@ -1,10 +1,3 @@
-# Make the code generator sources of dune-structures known
-# to dune-codegen to correctly retrigger code generation upon changes
-# in the python code.
-
-file(GLOB_RECURSE _ADDITIONAL_SOURCES "${CMAKE_SOURCE_DIR}/python/dune/structures/*.py")
-set(DUNE_CODEGEN_ADDITIONAL_PYTHON_SOURCES ${DUNE_CODEGEN_ADDITIONAL_PYTHON_SOURCES} ${_ADDITIONAL_SOURCES})
-
 # Search for additional packages needed by dune-structures
 find_package(ParMETIS 4)
 find_package(Gmsh)
